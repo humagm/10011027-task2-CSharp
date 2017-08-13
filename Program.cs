12335847c317cfe525a31ee0f594246c84e86f40
Milan Humagain <10011027@BCS.NET.NZ>
2017-08-14 11:56:52 +1200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _10011027_task1_JS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.Clear();
+
+            Console.WriteLine("please enter a number");
+
+            var input = Console.ReadLine();
+
+
+
+
+
+            var number = 0;
+
+            var isnum = int.TryParse (input, out number);
+
+            if(isnum){
+
+            int count = 10;
+
+            for (int i = 0; i <=count; i++)
+
+            Console.WriteLine(input + "X"  +i);
+             }
+
+             Console.ResetColor();
+
+             Console.WriteLine();
+
+             Console.WriteLine("Press <Enter> to quit the program");
+
+             Console.ReadKey();
+
+        
+        }
+    }
+}
+
+
+            
+
+            
+
+
+
+            
